71bfb7caa13a8bbecdd93658547e73774006724c
Jon Jee <jydrogen@gmail.com>
2019-07-14 21:14:36 +0900

    sleep 함수 추가, 로그인 버튼 입력하는 것까지 추가, 근데 이용자수 많다고 뜸 ㅜ

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const myAccount = require("./nike-account.js");
 
 const PAGE_INSTOCK = 'https://www.nike.com/kr/launch/?type=in-stock&activeDate=date-filter:BEFORE';
 
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
 (async () => {
     browser = await puppeteer.launch({ headless: false });
     page = await browser.newPage();
@@ -14,14 +18,28 @@ const PAGE_INSTOCK = 'https://www.nike.com/kr/launch/?type=in-stock&activeDate=d
 
     await page.goto(PAGE_INSTOCK);
 
-    loginBtnSelector = `a[title='로그인']`;
-    await page.click(loginBtnSelector);
+    goToLoginBtnSelector = `a[title='로그인']`;
+    await page.click(goToLoginBtnSelector);
 
+    // 이메일 입력
     emailInputSelector = `input#j_username`;
     await page.waitForSelector(emailInputSelector, {
         visible: true
     });
     await page.type(emailInputSelector, myAccount.email);
+
+    await sleep(1000);
+
+    // 비밀번호 입력
+    passwordInputSelector = `input#j_password`;
+    await page.type(passwordInputSelector, myAccount.password);
+
+    await sleep(1000);
+
+    // 로그인 버튼 입력
+    loginBtnSelector = `button.button.large.width-max`;
+    await page.click(loginBtnSelector);
+
 
     // await page.close();
     // await browser.close();
