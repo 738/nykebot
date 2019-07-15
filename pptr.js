5d629d206b024c9bf68332820b23078afcc108dc
Jon Jee <jydrogen@gmail.com>
2019-07-15 19:18:49 +0900

    pptr에서 chrome extension으로 방향 전환

--- pptr.js
+++ pptr.js
@@ -16,6 +16,18 @@ function sleep(ms) {
         height: 800
     });
 
+    await page.setUserAgent('Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36');
+
+    // await page.setRequestInterception(true);
+    // page.on('request', request => {
+    //     // Override headers
+    //     const headers = Object.assign({}, request.headers(), {
+    //         foo: 'bar', // set "foo" header
+    //         origin: undefined, // remove "origin" header
+    //     });
+    //     request.continue({ headers });
+    // });
+
     await page.goto(PAGE_INSTOCK);
 
     goToLoginBtnSelector = `a[title='로그인']`;
